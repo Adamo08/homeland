568a69ca2675cb858ce9b6ef7443055158201dca
Adamo0 <you@example.com>
2024-09-11 16:20:04 +0200

    Added Bootstrap CDN link

--- includes/footer.php
+++ includes/footer.php
@@ -80,6 +80,8 @@
 <script src="<?php echo URL('assets/js/circleaudioplayer.js') ?>"></script>
 
 <script src="<?php echo URL('assets/js/main.js') ?>"></script>
+<!-- Bootstrap JS Link  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
