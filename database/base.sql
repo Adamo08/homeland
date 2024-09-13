6c578bb3699ab9e27482268dfbe7af33340cf3c6
Adamo0 <you@example.com>
2024-09-13 03:44:14 +0200

    Added favorites and requests tables

--- database/base.sql
+++ database/base.sql
@@ -162,3 +162,33 @@ VALUES
     (5, 'property5_image3.jpg');
 
 
+
+-- The favorites table:
+CREATE TABLE IF NOT EXISTS favorites (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE (property_id, user_id) -- Prevent duplicate entries
+);
+
+
+CREATE TABLE IF NOT EXISTS requests (
+    id INT AUTO_INCREMENT PRIMARY KEY,       
+    name VARCHAR(255) NOT NULL,              
+    email VARCHAR(255) NOT NULL,             
+    phone VARCHAR(20),                       
+    user_id INT,                             -- ID of the user making the request (foreign key)
+    property_id INT,                         -- ID of the property related to the request (foreign key)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, 
+    FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE,
+    UNIQUE (property_id, user_id) -- Prevent duplicate entries
+);
+
+
+
+
