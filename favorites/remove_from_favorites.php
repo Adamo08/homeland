b9cac29738c4d5dd3915abb391774a52dc2c32eb
Adamo0 <you@example.com>
2024-09-13 03:45:07 +0200

    removing properties from favorites

---
+++ favorites/remove_from_favorites.php
@@ -0,0 +1,29 @@
+<?php 
+
+    if (!isset($_SESSION['user'])){
+        echo "<script> window.location.href = '".URL()."'</script>";
+        exit();
+    }
+
+?>
+
+<?php
+    require_once "../config/config.php";
+    require_once "../functions/database.php";
+
+    if (isset($_POST['property_id']) && isset($_POST['user_id'])) {
+        $propertyId = $_POST['property_id'];
+        $userId = $_POST['user_id'];
+
+
+        if (removeFromFavorites($userId,$propertyId)) {
+            $response = ['status' => 'success', 'message' => 'Property removed from favorites'];
+        } else {
+            $response = ['status' => 'error', 'message' => 'Failed to remove property'];
+        }
+    } else {
+        $response = ['status' => 'error', 'message' => 'Invalid request'];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
