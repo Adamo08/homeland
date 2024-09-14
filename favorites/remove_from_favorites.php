269d428f55f2f1fe9ac9bb3438e6a3398246455d
Adamo0 <you@example.com>
2024-09-14 02:44:48 +0200

    Updated remove_from_favorites.php

--- favorites/remove_from_favorites.php
+++ favorites/remove_from_favorites.php
@@ -1,15 +1,15 @@
-<?php 
 
+
+<?php
+
+    require_once "../config/config.php";
+    require_once "../functions/database.php";
+
+    session_start();
     if (!isset($_SESSION['user'])){
         echo "<script> window.location.href = '".URL()."'</script>";
         exit();
     }
-
-?>
-
-<?php
-    require_once "../config/config.php";
-    require_once "../functions/database.php";
 
     if (isset($_POST['property_id']) && isset($_POST['user_id'])) {
         $propertyId = $_POST['property_id'];
