ed147a9169ee79be1262fc2cb76fa32fee7339a5
Adamo0 <you@example.com>
2024-09-14 02:58:29 +0200

    Updated property-details.php

--- property-details.php
+++ property-details.php
@@ -457,7 +457,7 @@
           $(document).ready(function() {
               $('#add-to-favorites').on('click', function() {
                   var $form = $('#favorite-form');
-                  console.table($form.serialize());
+                  // console.table($form.serialize());
                   $.ajax({
                       url: $form.attr('action'),
                       method: 'POST',
