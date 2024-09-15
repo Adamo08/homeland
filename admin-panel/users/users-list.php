8dc7ba8e320cd0d6bd4c45eb311edc2bc7b5ad9d
Adamo0 <you@example.com>
2024-09-15 20:00:16 +0200

    Listing users

--- admin-panel/users/users-list.php
+++ admin-panel/users/users-list.php
@@ -1,11 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello From Users list</h1>
-</body>
-</html>
+<?php require_once "../includes/header.php"?>
+
+
+
+<?php 
+
+    if (!isset($_SESSION['admin_id'])){
+        echo "<script> window.location.href = '".ADMINAUTH."' </script>";
+    }
+
+    // Getting the list of users
+    $users = getUsers();
+    $i = 0;
+
+?>
+
+    <main>
+        <div class="container-fluid px-4">
+
+            <h1 class="mt-4">Users</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Users</li>
+            </ol>
+            <div class="container">
+                <table id="datatablesSimple" class="display">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Joined AT</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php if(!empty($users)):?>
+                            <?php foreach($users as $user):?>
+                                <tr>
+                                    <td><?=++$i?></td>
+                                    <td><?=$user['full_name']?></td>
+                                    <td><?=$user['username']?></td>
+                                    <td><?=$user['email']?></td>
+                                    <td><?=$user['phone']?></td>
+                                    <td><?=$user['address']?></td>
+                                    <td><?=$user['created_at']?></td>
+                                </tr>
+                            <?php endforeach;?>
+                        <?php endif;?>
+                        
+
+                    </tbody>
+                </table>
+            </div>
+
+            <a href="add-user.php" class="btn btn-primary my-3">Add New User</a>
+
+        </div>
+    </main>
+
+    <script>
+        window.addEventListener('DOMContentLoaded', event => {
+            // Simple-DataTables
+            // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+            const datatablesSimple = document.getElementById('datatablesSimple');
+            if (datatablesSimple) {
+                new simpleDatatables.DataTable(datatablesSimple);
+            }
+        });
+
+    </script>
+
+
+<?php require_once "../includes/footer.php"?>
