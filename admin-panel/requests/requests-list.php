c9cb45564a4aecfae2b2ff158d5ad4dc892a9c05
Adamo0 <you@example.com>
2024-09-15 20:00:04 +0200

    Listing user requests

--- admin-panel/requests/requests-list.php
+++ admin-panel/requests/requests-list.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello From Requests List</h1>
-</body>
-</html>
+<?php require_once "../includes/header.php"?>
+
+
+<main>
+    Requests
+</main>
+
+
+<?php require_once "../includes/footer.php"?>
