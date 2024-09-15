7ceb6139c2d188bd14f5d9da26cd9dd95732bf2e
Adamo0 <you@example.com>
2024-09-15 19:59:04 +0200

    Listing properties

--- admin-panel/properties/properties-list.php
+++ admin-panel/properties/properties-list.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello From Properties List</h1>
-</body>
-</html>
+<?php require_once "../includes/header.php"?>
+
+
+<main>
+    Properties
+</main>
+
+
+<?php require_once "../includes/footer.php"?>
