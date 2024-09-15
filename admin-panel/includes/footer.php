bee3e00e09adada88dac1161b6160af1a9592118
Adamo0 <you@example.com>
2024-09-15 19:58:47 +0200

    Removing unnecessary links

--- admin-panel/includes/footer.php
+++ admin-panel/includes/footer.php
@@ -24,10 +24,8 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo ADMINURL?>assets/js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo ADMINURL?>assets/demo/chart-area-demo.js"></script>
-        <script src="<?php echo ADMINURL?>assets/demo/chart-bar-demo.js"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo ADMINURL?>assets/js/datatables-simple-demo.js"></script>
     </body>
 </html>
