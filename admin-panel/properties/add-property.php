172a9e56542624600a669f6629528c67c199686e
Adamo0 <you@example.com>
2024-09-15 19:55:13 +0200

    Updated layout

--- admin-panel/properties/add-property.php
+++ admin-panel/properties/add-property.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello From Add Property</h1>
-</body>
-</html>
+<?php require_once "../includes/header.php"?>
+
+
+<main>
+    Add Property
+</main>
+
+
+<?php require_once "../includes/footer.php"?>
