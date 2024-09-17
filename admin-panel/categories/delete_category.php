8ea3a0043f8a8e18be075889ea29554424dd8b41
Adamo0 <you@example.com>
2024-09-17 22:06:22 +0200

    Deleting categories

---
+++ admin-panel/categories/delete_category.php
@@ -0,0 +1,43 @@
+<?php
+    include "../helpers/helpers.php";
+    include "../../functions/database.php";
+
+    session_start();
+    if(!isset($_SESSION['admin_id'])){
+        // Go login bro
+        echo "<script> window.location.href = '".ADMINAUTH."login.php' </script>";
+        exit();
+    }
+
+    
+    $response = [
+        'status' => 'error', 
+        'message' => 'Invalid request'
+    ];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        $id = isset($_POST['category_id']) ? intval($_POST['category_id']) : 0;
+        
+        if ($id > 0){
+            // Updating category
+            $deleteSuccess = deleteCategory($id);
+            
+            if ($deleteSuccess) {
+                $response = ['status' => 'success', 'message' => 'Category deleted successfully!'];
+            } else {
+                $response = ['status' => 'error', 'message' => 'Failed to delete category.'];
+            }
+        }
+        else
+        {
+            $response = [
+                'status' => 'error', 
+                'message' => 'Missing parameters.'
+            ];
+        }
+
+
+    }
+
+    header('Content-type: application/json');
+    echo json_encode($response);
