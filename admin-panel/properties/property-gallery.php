07020dd8562934d4cd3c502f0a3f4907a43047ea
Adamo0 <you@example.com>
2024-09-20 23:36:53 +0200

    Setting up the property gallery page

---
+++ admin-panel/properties/property-gallery.php
@@ -0,0 +1,88 @@
+<?php require_once "../includes/header.php"?>
+
+
+<div class="container pb-5">
+    <h1 class="my-4 font-weight-bold bg-primary text-white px-3 py-2 rounded-3 text-center">Property Gallery</h1>
+    <ol class="breadcrumb mb-4 d-flex justify-content-between align-items-center">
+        <li class="breadcrumb-item active">Gallery</li>
+        <a href="properties-list.php" class="btn btn-primary">Add Photo</a>
+    </ol>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+        <!-- Gallery Item 1 -->
+        <div class="col">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property1.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property1.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 1"
+                    >
+                </a>
+            </div>
+        </div>
+        <!-- Gallery Item 2 -->
+        <div class="col col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property2.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property2.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 2"
+                    >
+                </a>
+            </div>
+        </div>
+        <!-- Gallery Item 3 -->
+        <div class="col col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property3.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property3.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 3"
+                    >
+                </a>
+            </div>
+        </div>
+        <!-- Gallery Item 4 -->
+        <div class="col col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property4.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property4.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 4"
+                    >
+                </a>
+            </div>
+        </div>
+        <!-- Gallery Item 5 -->
+        <div class="col col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property5.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property5.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 5"
+                    >
+                </a>
+            </div>
+        </div>
+        <!-- Gallery Item 6 -->
+        <div class="col col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <a href="http://localhost/Homeland/assets/uploads/properties/images/property6.jpg" class="image-popup gal-item">
+                    <img 
+                        src="http://localhost/Homeland/assets/uploads/properties/images/property6.jpg" 
+                        class="img-fluid img-thumbnail h-100" 
+                        alt="Property Image 6"
+                    >
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php require_once "../includes/footer.php"?>
